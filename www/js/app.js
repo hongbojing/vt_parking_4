af4a90bc228bfc4500df1923f23cf0a91f969478
hongbojing <jack.jing7@gmail.com>
2017-04-06 14:35:33 -0400

    1st commit

---
+++ www/js/app.js
@@ -0,0 +1,64 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'MapCtrl'
+        }
+      }
+    });
+
+    $stateProvider
+      .state('congratulations', {
+      url: '/congratulations',
+      templateUrl: 'templates/congratulations.html'
+    });
+
+    $stateProvider
+      .state('loggedin', {
+      url: '/loggedin',
+      templateUrl: 'templates/welcome.html'
+    });
+
+    $stateProvider
+      .state('addlocations', {
+      url: '/addlocations',
+      templateUrl: 'templates/add-locations.html'
+    });
+
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,46 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('MapCtrl', function($scope, $ionicLoading) {
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+});
