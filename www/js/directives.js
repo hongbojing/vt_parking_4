692608426c5cfdd053f46af607e7bfbce570ca8c
hongbojing <jack.jing7@gmail.com>
2017-04-13 13:38:19 -0400

    xx

--- www/js/directives.js
+++ www/js/directives.js
@@ -34,24 +34,20 @@ angular.module('starter.directives', [])
 
         };
         var myLatLng = {lat: 37.231213, lng: -80.426263};
-        var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        var image = {
+            url: 'img/marker_image_1.png',
+            size: new google.maps.Size(75, 73),
+            anchor: new google.maps.Point(0, 32)
+        };
         var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<h1 id="firstHeading" class="firstHeading">Parking Structure Perry Street Lot</h1>'+
             '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '<p><button>Favorited</button> <button>Directions 7 min</button>' +
+            'random words random words random words random words.</p>'+
+            '<p>random link, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'Random link</a> '+
             '(last visited June 22, 2009).</p>'+
             '</div>'+
             '</div>';
@@ -75,8 +71,8 @@ angular.module('starter.directives', [])
         });
 
         marker.addListener('click', function() {
-        //   infowindow.open(map, marker);
-          $scope.show();
+          infowindow.open(map, marker);
+        //   $scope.show();
         });
 
         $scope.onCreate({map: map});
