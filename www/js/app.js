1fba4ff02c013ec66624fcfc18c3b0c46e7c83e8
Caicai Zhang <hongboing@Hongbo-Jings-MacBook-Pro.local>
2017-04-16 13:39:26 -0400

    stop point 1

--- www/js/app.js
+++ www/js/app.js
@@ -16,49 +16,73 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives']
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
+  /*
+  最开始建立的基准的state,是一个会随着其他dot之后的内容一起走的.
+   */
   $stateProvider
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-  .state('tab.dash', {
-    url: '/chats',
+    /*
+    state 的第一个小分支
+    #1: log-in
+     */
+  .state('tab.login', {
+    url: '/1-log-in',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-login': {
+        templateUrl: 'templates/1-log-in.html',
+        controller: 'LoginCtrl'//need to be changed to LoginCtrl
       }
     }
   })
-  .state('tab.chats', {
-      url: '/dash',
+  /*
+    state 的第二个小分支
+    #7: parking-map
+     */
+  .state('tab.parkingmap', {
+      url: '/7-parking-map',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
+        'tab-parkingmap': {
+          templateUrl: 'templates/7-parking-map.html',
           controller: 'MapCtrl'
         }
       }
     });
 
+    /*
+    一个state对应着一个路径, 也对应着一个页面
+    #6: personal settings
+     */
     $stateProvider
       .state('congratulations', {
-      url: '/congratulations',
-      templateUrl: 'templates/congratulations.html'
+      url: '/6-personal-setting',
+      templateUrl: 'templates/6-personal-setting.html'
     });
 
+    /*
+    #2: home-page
+     */
     $stateProvider
-      .state('loggedin', {
-      url: '/loggedin',
-      templateUrl: 'templates/welcome.html'
+      .state('homepage', {
+      url: '/2-home-page',
+      templateUrl: 'templates/2-home-page.html'
     });
 
+    /*
+    #4: home-address
+     */
     $stateProvider
-      .state('addlocations', {
-      url: '/addlocations',
-      templateUrl: 'templates/add-locations.html'
+      .state('homeaddress', {
+      url: '/4-home-address',
+      templateUrl: 'templates/4-home-address.html'
     });
 
-  $urlRouterProvider.otherwise('/tab/dash');
+  /*
+  定义默认的路径
+   */
+  $urlRouterProvider.otherwise('/tab/1-log-in');
 
 });
