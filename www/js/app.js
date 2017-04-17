f2feff8946e8c6ac56e4ba092577f031d70807c3
Caicai Zhang <hongboing@Hongbo-Jings-MacBook-Pro.local>
2017-04-17 00:53:26 -0400

    map is the last mission

--- www/js/app.js
+++ www/js/app.js
@@ -53,6 +53,43 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
     });
 
     /*
+     #2: home-page
+     */
+    $stateProvider
+      .state('homepage', {
+        url: '/2-home-page',
+        templateUrl: 'templates/2-home-page.html',
+        controller: 'bottomSheetController'
+      });
+
+    /*
+     #3: system-setting
+     */
+    $stateProvider
+      .state('systemsetting', {
+        url: '/3-system-setting',
+        templateUrl: 'templates/3-system-setting.html'
+      });
+
+    /*
+     #4: home-address
+     */
+    $stateProvider
+      .state('homeaddress', {
+        url: '/4-home-address',
+        templateUrl: 'templates/4-home-address.html'
+      });
+
+    /*
+     #5: favor-parking
+     */
+    $stateProvider
+      .state('favorparking', {
+        url: '/5-favor-parking',
+        templateUrl: 'templates/5-favor-parking.html'
+      });
+
+    /*
     一个state对应着一个路径, 也对应着一个页面
     #6: personal settings
      */
@@ -60,25 +97,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
       .state('congratulations', {
       url: '/6-personal-setting',
       templateUrl: 'templates/6-personal-setting.html'
-    });
-
-    /*
-    #2: home-page
-     */
-    $stateProvider
-      .state('homepage', {
-      url: '/2-home-page',
-      templateUrl: 'templates/2-home-page.html',
-      controller: 'bottomSheetController'
-    });
-
-    /*
-    #4: home-address
-     */
-    $stateProvider
-      .state('homeaddress', {
-      url: '/4-home-address',
-      templateUrl: 'templates/4-home-address.html'
     });
 
   /*
